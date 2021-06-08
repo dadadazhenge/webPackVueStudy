deff477e56a4752c309be818216f5f491320e637
xiaoli <815184693@qq.com>
2021-06-08 18:10:28 +0800

    今天必须完成

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,48 +1,5 @@
 <template>
     <div>
-        <ul style="list-style: none">
-            <li style="width: 180px; height: 35px">
-                <item :isActive="curActive==='xianhuoping'" @active="curActive='xianhuoping'">
-                    <span>
-                        <img src="http://121.36.89.165:8080/shopping/resources/style/common/images/icon/default_icon.png" alt="">
-                    </span>
-                    <span>鲜活品</span>
-                </item>
-            </li>
-            <li style="width: 180px; height: 35px">
-                <item :isActive="curActive==='lengdongping'" @active="curActive='lengdongping'">
-                    <span>
-                        <img src="http://121.36.89.165:8080/shopping/resources/style/common/images/icon/icon_2.png" alt="">
-                    </span>
-                    <span>冷冻品</span>
-                </item>
-            </li>
-            <li style="width: 180px; height: 35px">
-                <item :isActive="curActive==='ganzhiping'" @active="curActive='ganzhiping'">
-                    <span>
-                        <img src="http://121.36.89.165:8080/shopping/resources/style/common/images/icon/icon_3.png" alt="">
-                    </span>
-                    <span>干制品</span>
-                </item>
-            </li>
-            <li style="width: 180px; height: 35px">
-                <item :isActive="curActive==='yanzhiping'" @active="curActive='yanzhiping'">
-                    <span>
-                        <img src="http://121.36.89.165:8080/shopping/resources/style/common/images/icon/icon_5.png" alt="">
-                    </span>
-                    <span>腌制品</span>
-                </item>
-            </li>
-            <li style="width: 180px; height: 35px">
-                <item :isActive="curActive==='yumijiyunizhiping'" @active="curActive='yumijiyunizhiping'">
-                    <span>
-                        <img src="http://121.36.89.165:8080/shopping/resources/style/common/images/icon/icon_7.png" alt="">
-                    </span>
-                    <span>鱼糜及鱼糜制品</span>
-                </item>
-            </li>
-        </ul>
-
         <!-- <router-link to='/login'>登录</router-link>
         <router-link to='/registry'>注册</router-link>
         <router-view></router-view> -->
@@ -50,33 +7,14 @@
 </template>
 
 <script>
-    import item from './subComponents/item.vue';
-
     export default{
-        data(){
-            return {
-                curActive: 'xianhuoping'
-            }
-        },
-        components: {
-            item
-        },
+        
     }
 </script>
 
 <style scoped>
-
-    .box{
-        width: 180px;
-        height: 35px;
-    }
-
     *{
         margin: 0;
         padding: 0;
-    };
-
-    a{
-        text-decoration: none;
     };
 </style>
---
+++ src/components/subComponents/itemList.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <ul style="list-style: none">
+            <li style="width: 180px; height: 35px">
+                <item :isActive="curActive==='xianhuoping'" @active="curActive='xianhuoping'">
+                    <span>
+                        <img :src="dataArry.src" alt="">
+                    </span>
+                    <span>{{ dataArry.name }}</span>
+                </item>
+            </li>
+    </div>
+</template>
+
+<script>
+import item from './itemList.vue';
+
+export default {
+    data(){
+        return {
+            curActive: 'xianhuoping'
+        }
+    },
+    components: {
+        item
+    },
+    props: {
+        dataArry: {
+            type: Object,
+            default: function(){
+                return {};
+            }
+        }
+    }
+}
+</script>
+
+<style>
+    
+</style>
